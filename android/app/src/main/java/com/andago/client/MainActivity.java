ae20d35283576d98713ff4636db98a830a911afb
Kottalo <kottaloflotenspieler@gmail.com>
2023-04-12 13:30:08 +0800

    Test debug

--- android/app/src/main/java/com/andago/client/MainActivity.java
+++ android/app/src/main/java/com/andago/client/MainActivity.java
@@ -1,14 +1,22 @@
 package com.andago.client;
 
-import android.webkit.WebView;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
+import android.webkit.WebView;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    // Enable WebViews debugging
+    // Enable web contents debugging
     WebView.setWebContentsDebuggingEnabled(true);
+
+    // Initialize the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins can be included here
+    }});
   }
 }
