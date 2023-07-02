4f0e913086d2f0a6b5b5473afdb46b2e2d981e14
Kottalo <kottaloflotenspieler@gmail.com>
2023-07-03 02:04:01 +0800

    Test RTC

--- src/views/test/TestMain.vue
+++ src/views/test/TestMain.vue
@@ -7,9 +7,15 @@
     </ion-header>
 
     <ion-content>
-      <ion-button @click="startBrowser">
-        Start Browser
-      </ion-button>
+      <h2 class="left-align">Get started with Voice Calling</h2>
+      <div class="row">
+        <div>
+          <ion-button type="button" id="join" @click="join">Join</ion-button>
+          <ion-button type="button" id="leave" @click="leave">Leave</ion-button>
+        </div>
+      </div>
+      <br>
+      <div id="message"></div>
     </ion-content>
   </ion-page>
 </template>
@@ -18,11 +24,79 @@
 import {
   IonButton, IonPage, IonHeader, IonToolbar, IonTitle, IonContent,
 } from '@ionic/vue';
-import { useMainStore } from '@/store';
+import AgoraRTC from "agora-rtc-sdk-ng"
 
-const store = useMainStore();
+const options =
+{
+  // Pass your App ID here.
+  appId: 'd136d74a947d46de9a99a072ac9d57a6',
+  // Set the channel name.
+  channel: 'test',
+  // Pass your temp token here.
+  token: '007eJxTYDD9Es534+00oW12QR8U9/LfbjGqm8u6bEN2nHDP4oZHcVoKDCmGxmYp5iaJlibmKSZmKamWiZaWiQbmRonJlimm5olm99cvTGkIZGSo5G9lZGSAQBCfhaEktbiEgQEAMyofOw==',
+  // Set the user ID.
+  uid: 0,
+};
 
-function startBrowser() {
-  store.openMap()
+const channelParameters =
+  {
+    // A variable to hold a local audio track.
+    localAudioTrack: null,
+    // A variable to hold a remote audio track.
+    remoteAudioTrack: null,
+    // A variable to hold the remote user id.
+    remoteUid: null,
+  } as any;
+
+const agoraEngine = AgoraRTC.createClient({ mode: "rtc", codec: "vp9" });
+
+async function startBasicCall() {
+  // Create an instance of the Agora Engine
+
+
+  // Listen for the "user-published" event to retrieve an AgoraRTCRemoteUser object.
+  agoraEngine.on("user-published", async (user, mediaType) => {
+    // Subscribe to the remote user when the SDK triggers the "user-published" event.
+    await agoraEngine.subscribe(user, mediaType);
+    console.log("subscribe success");
+
+    // Subscribe and play the remote audio track.
+    if (mediaType == "audio") {
+      channelParameters.remoteUid = user.uid;
+      // Get the RemoteAudioTrack object from the AgoraRTCRemoteUser object.
+      channelParameters.remoteAudioTrack = user.audioTrack;
+      // Play the remote audio track. 
+      channelParameters.remoteAudioTrack.play();
+      // showMessage("Remote user connected: " + user.uid);
+    }
+
+    // Listen for the "user-unpublished" event.
+    agoraEngine.on("user-unpublished", user => {
+      console.log(user.uid + "has left the channel");
+      // showMessage("Remote user has left the channel");
+    });
+  });
 }
+
+async function join() {
+  // Join a channel.
+  await agoraEngine.join(options.appId, options.channel, options.token, options.uid);
+  // showMessage("Joined channel: " + options.channel);
+  // Create a local audio track from the microphone audio.
+  channelParameters.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+  // Publish the local audio track in the channel.
+  await agoraEngine.publish(channelParameters.localAudioTrack);
+  console.log("Publish success!");
+}
+
+async function leave() {
+  // Destroy the local audio track.
+  channelParameters.localAudioTrack.close();
+  // Leave the channel
+  await agoraEngine.leave();
+  console.log("You left the channel");
+}
+
+startBasicCall();
+
 </script>
